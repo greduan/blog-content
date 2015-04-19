938c0088addc6851093ae85536794b839e67b708
greduan <me@greduan.com>
2015-04-19 13:27:27 -0500

    Templates (layout->template, post->post.html)

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict'
 
 var Metalsmith = require('metalsmith'),
-	md = require('metalsmith-markdown')
+	md = require('metalsmith-markdown'),
+	temp = require('metalsmith-templates')
 
 var ms = Metalsmith(__dirname)
+.source('./_posts')
+.destination('./_site')
 .use(md())
+.use(temp({
+	engine: 'swig',
+	directory: './_layouts'
+}))
 .build(function (err) {
 	if (err) {
 		throw err;
