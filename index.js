7659335ffa44d02a564ef8f7770441ae3e23ba39
greduan <me@greduan.com>
2015-04-19 14:32:40 -0500

    CSS and a bit of feed

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 var Metalsmith = require('metalsmith'),
 	md = require('metalsmith-markdown'),
-	temp = require('metalsmith-templates')
+	temp = require('metalsmith-templates'),
+	path = require('metalsmith-path'),
+	coll = require('metalsmith-collections')
 
 var ms = Metalsmith(__dirname)
 .source('./src')
 .destination('./_site')
 .use(md())
+.use(path())
+.use(coll({
+	posts: {
+		patterh: '*.html',
+		sortBy: 'date',
+	}
+}))
 .use(temp({
 	engine: 'swig',
-	directory: 'templates'
+	directory: 'templates',
 }))
 .build(function (err) {
 	if (err) {
-		throw err;
+		throw err
 	}
 })
