42e95b488fe7b26f0aa4eaa5457dee31e1a650cb
greduan <me@greduan.com>
2015-09-01 15:56:48 -0500

    two months of openbsd blog post.  chmod -x

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 
 var Metalsmith = require('metalsmith'),
-	drafts = require('metalsmith-drafts'),
-	md = require('metalsmith-markdown'),
-	temp = require('metalsmith-templates'),
-	path = require('metalsmith-path'),
-	coll = require('metalsmith-collections')
+  drafts = require('metalsmith-drafts'),
+  md = require('metalsmith-markdown'),
+  temp = require('metalsmith-templates'),
+  path = require('metalsmith-path'),
+  coll = require('metalsmith-collections')
 
 var ms = Metalsmith(__dirname)
 .source('./src')
@@ -15,18 +15,18 @@ var ms = Metalsmith(__dirname)
 .use(path())
 /*
 .use(coll({
-	posts: {
-		patterh: '*.html',
-		sortBy: 'date',
-	}
+  posts: {
+    patterh: '*.html',
+    sortBy: 'date',
+  }
 }))
 */
 .use(temp({
-	engine: 'swig',
-	directory: 'templates',
+  engine: 'swig',
+  directory: 'templates',
 }))
 .build(function (err) {
-	if (err) {
-		throw err
-	}
+  if (err) {
+    throw err
+  }
 })
