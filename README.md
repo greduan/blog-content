0059f8a47e42102bfe104aaae85a72eb1779044c
greduan <me@greduan.com>
2018-02-17 00:42:29 +0100

    Return plain text GPG

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 Source code for https://greduan.com
 
 - https://blog.greduan.com => https://greduan.com/blog
-- https://projects.greduan.com => https://greduan.com/projects
 - https://www.greduan.com => https://greduan.com
 - https://gpg.greduan.com => https://greduan.com/gpg
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const path = require('path')
 const fastify = require('fastify')
 const fastifyStatic = require('fastify-static')
+const fs = require('fs')
 
 const app = fastify({ logger: true })
+
+const gpg = fs.readFileSync(path.join(__dirname, 'gpg-pub.txt'), 'utf8')
+
+app.get('/gpg', (request, reply) => {
+  reply
+    .code(200)
+    .header('Content-Type', 'text/plain; charset=us-ascii')
+    .send(gpg)
+})
 
 app.register(fastifyStatic, {
   root: path.join(__dirname, 'dist'),
