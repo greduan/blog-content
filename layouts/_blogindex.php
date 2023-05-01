cdc668817adc29b3db21e2a1d2cab6b7bdc75f77
greduan <me@greduan.com>
2023-05-01 22:13:02 +0200

    Refactor blog posts list into function

---
+++ layouts/_blogindex.php
@@ -0,0 +1,63 @@
+<?php
+function get_blog_posts() {
+  // Get list of posts
+  $offset = strlen(dirname(__FILE__, 2));
+  $posts = glob(dirname(__FILE__, 2) . '/blog/*/*/*/*.md');
+
+  // Get metadata for posts
+  $files = array_map(function ($full_post_path) use ($offset) {
+    $post_uri = substr($full_post_path, $offset);
+    $file = new File($post_uri);
+    $metadata = $file->get_metadata();
+    $path_parts = pathinfo($post_uri);
+    $metadata['_uri'] = $path_parts['dirname'] . '/' . $path_parts['filename'];
+    return $metadata;
+  }, $posts);
+
+  // Sort in reverse order, that is to say newer dates first.
+  usort($files, function ($a, $b) {
+    if ($a['date'] === $b['date']) {
+      return 0;
+    }
+    // ? -1 : 1 is normal order
+    return ($a['date'] < $b['date']) ? 1 : -1;
+  });
+
+  return $files;
+}
+
+function render_blog_posts($only_first_x) {
+  $posts = get_blog_posts();
+  if ($only_first_x > 0) {
+    $posts = array_slice($posts, 0, $only_first_x);
+  }
+
+  // Group posts by year
+  $years = array_reduce($posts, function ($carry, $post) {
+    $year = date('Y', $post['date']);
+    if (!isset($carry[$year])) {
+      $carry[$year] = [];
+    }
+    $carry[$year][] = $post;
+    return $carry;
+  }, []);
+  ?>
+<div id="blogindex">
+  <?php foreach ($years as $year => $posts) { ?>
+    <h3><?php echo $year; ?></h3>
+    <?php foreach ($posts as $post) { ?>
+      <div class="blogpost">
+        <div class="date">
+          <time datetime="<?php echo date('Y-m-d', $post['date']); ?>">
+            <?php echo date('Y-m-d', $post['date']); ?>
+          </time>
+        </div>
+        <div class="link">
+          <a href="<?php echo $post['_uri']; ?>"><?php echo $post['title']; ?></a>
+        </div>
+      </div>
+    <?php } ?>
+  <?php } ?>
+</div>
+  <?php
+}
--- layouts/blogindex.php
+++ layouts/blogindex.php
@@ -1,34 +1,9 @@
 <?php
 require_once dirname(__FILE__) . '/_header.php';
+require_once dirname(__FILE__) . '/_blogindex.php';
 require_once dirname(__FILE__) . '/_footer.php';
 
 class Layout {
-  private function get_blog_posts() {
-    // Get list of posts
-    $offset = strlen(dirname(__FILE__, 2));
-    $posts = glob(dirname(__FILE__, 2) . '/blog/*/*/*/*.md');
-
-    // Get metadata for posts
-    $files = array_map(function ($full_post_path) use ($offset) {
-      $post_uri = substr($full_post_path, $offset);
-      $file = new File($post_uri);
-      $metadata = $file->get_metadata();
-      $path_parts = pathinfo($post_uri);
-      $metadata['_uri'] = $path_parts['dirname'] . '/' . $path_parts['filename'];
-      return $metadata;
-    }, $posts);
-
-    // Sort in reverse order, that is to say newer dates first.
-    usort($files, function ($a, $b) {
-      if ($a['date'] === $b['date']) {
-        return 0;
-      }
-      // ? -1 : 1 is normal order
-      return ($a['date'] < $b['date']) ? 1 : -1;
-    });
-
-    return $files;
-  }
 
   public function render($metadata) {
     ?>
@@ -44,20 +19,7 @@ class Layout {
 </head>
 <body>
 <?php render_header(); ?>
-<div id="blogindex">
-  <?php foreach ($this->get_blog_posts() as $post) { ?>
-    <div class="blogpost">
-      <div class="date">
-        <time datetime="<?php echo date('Y-m-d', $post['date']); ?>">
-          <?php echo date('Y-m-d', $post['date']); ?>
-        </time>
-      </div>
-      <div class="link">
-        <a href="<?php echo $post['_uri']; ?>"><?php echo $post['title']; ?></a>
-      </div>
-    </div>
-  <?php } ?>
-</div>
+<?php render_blog_posts(0); ?>
 <?php render_footer(); ?>
 </body>
 </html>
